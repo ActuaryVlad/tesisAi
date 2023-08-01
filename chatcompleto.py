cded7f4c1c6731ee67634514fa948dac721ca7f8
PAPIVLAD <Montero.vladimir13@gmail.com>
2023-08-01 17:02:21 -0400

    de listo

--- chatcompleto.py
+++ chatcompleto.py
@@ -8,7 +8,8 @@ from sklearn.preprocessing import LabelEncoder, StandardScaler, MinMaxScaler, Ro
 from sklearn.tree import DecisionTreeClassifier, export_graphviz
 import graphviz
 from sklearn.model_selection import RandomizedSearchCV
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, roc_auc_score, recall_score, f1_score
+
 
 
 train = pd.read_csv('C:/Users/monte/Desktop/tesis/train.csv')
@@ -74,3 +75,41 @@ print("Mejores parámetros: ", random_search.best_params_)
 best_model = random_search.best_estimator_
 test_accuracy = best_model.score(x_test, y_test)
 print("Precisión en el conjunto de prueba: ", test_accuracy)
+
+#Guardar el arbol en pdf 
+dot_data = export_graphviz(best_model, out_file=None, feature_names=train.columns,
+                           class_names=['No', 'Yes'], filled=True, rounded=True, special_characters=True)
+graph = graphviz.Source(dot_data)
+graph.render("Decision_Tree")  # Esto guardará el árbol en un archivo llamado "Decision_Tree"
+
+dot_data = export_graphviz(best_model, max_depth = 3,
+                      out_file=None, 
+                      feature_names=train.columns,       # Cambiado de X.columns a train.columns
+                      class_names=['Yes','No'],
+                      filled=True, rounded=True,
+                      special_characters=True)  
+graph = graphviz.Source(dot_data)  
+graph
+
+# Obtener las probabilidades de la clase positiva
+y_probs = best_model.predict_proba(x_test)[:, 1]
+
+# Calcular el AUC-ROC
+auc_roc = roc_auc_score(y_test, y_probs)
+print("AUC-ROC:", auc_roc)
+
+# Predicciones en el conjunto de prueba
+y_pred = best_model.predict(x_test)
+
+# Calcular métricas
+accuracy = accuracy_score(y_test, y_pred)
+conf_matrix = confusion_matrix(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
+
+print("Accuracy:", accuracy)
+print("Confusion Matrix:\n", conf_matrix)
+print("Precision:", precision)
+print("Recall:", recall)
+print("F1 Score:", f1)
